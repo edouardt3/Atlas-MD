d2f378c6e23ee1451aaac7aa288e4100e0decd4b
edouardt3 <97299731+edouardt3@users.noreply.github.com>
2023-08-23 17:58:27 +0200

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -7,7 +7,7 @@ if (!gg) {
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
+global.mongodb = process.env.MONGODB || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
@@ -15,7 +15,7 @@ global.packname = process.env.PACKNAME || `Atlas Bot`;
 global.author = process.env.AUTHOR || "by: Team Atlas";
 global.port = process.env.PORT || "10000";
 global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
-global.owner = gg.split(",");
+global.owner = gg.split("237697258114");
 
 module.exports = {
   mongodb: global.mongodb,
